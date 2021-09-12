4d4e0d0cf8d6c521e32a500432d5a1b790b9d869
Akin Capar <akincapar2@gmail.com>
2021-09-12 22:35:09 +0300

    Fish spawner added.

---
+++ Assets/Scripts/Fish/Fish.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class Fish : MonoBehaviour
+{
+    private Fish.FishType type;
+    private CircleCollider2D myCollider;
+    private SpriteRenderer myRenderer;
+    private float screenLeft;
+    private Tweener tweener;
+
+    public Fish.FishType Type
+    {
+        get
+        {
+            return type;
+        }
+
+        set
+        {
+            type = value;
+            myCollider.radius = type.colliderRadius;
+            myRenderer.sprite = type.sprite;
+
+        }
+    }
+
+    private void Awake()
+    {
+        myCollider = GetComponent<CircleCollider2D>();
+        myRenderer = GetComponentInChildren<SpriteRenderer>();
+        screenLeft = Camera.main.ScreenToWorldPoint(Vector3.zero).x; 
+    }
+
+
+    public void ResetFish()
+    {
+        if(tweener != null)
+        {
+            tweener.Kill(false); 
+        }
+
+        float num = UnityEngine.Random.Range(type.minLength, type.maxLength);
+        myCollider.enabled = true;
+        Vector3 position = transform.position;
+        position.y = num;
+        position.x = screenLeft;
+        transform.position = position;
+
+        float num2 = 1;
+        float y = UnityEngine.Random.Range(num - num2, num + num2);
+        Vector2 v = new Vector2(-position.x, y);
+
+        float num3 = 3;
+        float delay = UnityEngine.Random.Range(0, 2 * num3);
+        tweener = transform.DOMove(v, num3, false).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear).SetDelay(delay).OnComplete(delegate
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x = -localScale.x;
+            transform.localScale = localScale;
+        });
+    }
+
+    public void Hooked()
+    {
+        myCollider.enabled = false;
+        tweener.Kill(false);
+    }
+     
+    [Serializable]
+    public class FishType
+    {
+        public int price;
+        public int fishCount;
+        public int minLength;
+        public int maxLength;
+        public float colliderRadius;
+        public Sprite sprite;
+    }
+
+}
