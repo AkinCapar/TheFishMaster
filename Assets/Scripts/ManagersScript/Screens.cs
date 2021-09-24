6bc100d0e930cf44b1d462f0b12f4ad9bda3b5e0
Akin Capar <akincapar2@gmail.com>
2021-09-24 17:39:36 +0300

    no message

---
+++ Assets/Scripts/ManagersScript/Screens.cs
@@ -0,0 +1,8 @@
+﻿ public enum Screens
+{
+    MAIN,
+    END,
+    GAME,
+    RETURN
+}
+
---
+++ Assets/Scripts/ManagersScript/ScreensManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreensManager : MonoBehaviour
+{
+    public static ScreensManager instance;
+
+    public GameObject currentScreen;
+
+    public GameObject endScreen;
+    public GameObject gameScreen;
+    public GameObject mainScreen;
+    public GameObject returnScreen;
+
+    public Button lengthButton;
+    public Button strengthButton;
+    public Button offlineButton;
+
+    public Text gameScreenMoney;
+    public Text lengthCostText;
+    public Text lengthValueText;
+    public Text strengthCostText;
+    public Text strengthValueText;
+    public Text offlineCostText;
+    public Text offlineValueText;
+    public Text endScreenMoney;
+    public Text returnScreenMoney;
+
+
+    private int gameCount;
+
+    private void Awake()
+    {
+        if(ScreensManager.instance)
+        {
+            Destroy(base.gameObject);
+        }
+
+        else
+        {
+            ScreensManager.instance = this;
+        }
+
+        currentScreen = mainScreen;
+    }
+
+    private void Start()
+    {
+        CheckIdles();
+        UpdateTexts();
+    }
+
+    public void ChangeScreens(Screens screen)
+    {
+        currentScreen.SetActive(false);
+        switch (screen)
+        {
+            case Screens.MAIN:
+                currentScreen = mainScreen;
+                UpdateTexts();
+                CheckIdles();
+                break;
+
+            case Screens.GAME:
+                currentScreen = gameScreen;
+                gameCount++
+                break;
+
+            case Screens.END:
+                currentScreen = endScreen;
+                //SetEndScreenMoney();
+                break;
+
+            case Screens.RETURN:
+                currentScreen = returnScreen;
+                //SetReturnScreenMoney();
+                break;
+
+        }
+    }
+
+    public void UpdateTexts()
+    {
+
+    }
+
+    public void CheckIdles()
+    {
+
+    }
+}
